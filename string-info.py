a3b1600b03784e4ec12b6a61f8c0da8c554c3ca4
LJO <chandralekhaa@gmail.com>
2023-02-02 11:51:51 -0500

    side-learning

---
+++ string-info.py
@@ -0,0 +1,10 @@
+#string menuplation
+
+message = input("Enter a message: ")
+
+print("First character: ", message[0])
+print("Last character: ", message[-1])
+print("Middle character: ", message[int(len(message) / 2)])
+print("Even index characters: ", message[0::2])
+print("odd index characters: ", message[1::2])
+print("reverse message: ", message[::-1])
